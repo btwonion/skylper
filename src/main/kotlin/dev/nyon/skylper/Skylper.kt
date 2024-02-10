bcc9f9989b1072d0579b3609e56445be424ca3e2
btwonion <btwonion@gmx.de>
2024-02-10 21:49:29 +0100

    update to konfig 2.0.0

--- src/main/kotlin/dev/nyon/skylper/Skylper.kt
+++ src/main/kotlin/dev/nyon/skylper/Skylper.kt
@@ -4,15 +4,12 @@ import dev.nyon.konfig.config.config
 import dev.nyon.konfig.config.loadConfig
 import dev.nyon.skylper.config.Config
 import dev.nyon.skylper.config.configDir
+import dev.nyon.skylper.config.configJsonBuilder
 import dev.nyon.skylper.config.migrate
 import dev.nyon.skylper.extensions.FabricEvents
 import dev.nyon.skylper.skyblock.data.session.PlayerSessionData
 import dev.nyon.skylper.skyblock.data.skylper.*
 import dev.nyon.skylper.skyblock.hollows.HollowsModule
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.*
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.Minecraft
@@ -24,11 +21,6 @@ lateinit var minecraft: Minecraft
 val independentScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
 object Skylper : ClientModInitializer {
-    val ktorClient = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json()
-        }
-    }
 
     @Suppress("SpellCheckingInspection")
     override fun onInitializeClient() {
@@ -36,10 +28,14 @@ object Skylper : ClientModInitializer {
         mcDispatcher = minecraft.asCoroutineDispatcher()
         mcScope = CoroutineScope(SupervisorJob() + mcDispatcher)
 
-        config(configDir.resolve("skylper.json"), 1, Config()) { jsonTree, version -> migrate(jsonTree, version) }
+        config(
+            configDir.resolve("skylper.json"), 1, Config(), configJsonBuilder::invoke
+        ) { jsonTree, version -> migrate(jsonTree, version) }
         internalConfig = loadConfig<Config>() ?: error("No config settings provided to load config!")
 
-        config(configDir.resolve("playerdata.json"), 1, StoredPlayerData()) { jsonTree, version ->
+        config(
+            configDir.resolve("playerdata.json"), 1, StoredPlayerData(), configJsonBuilder::invoke
+        ) { jsonTree, version ->
             migrateStoredPlayerData(jsonTree, version)
         }
         playerData = loadConfig<StoredPlayerData>() ?: error("No config settings provided to load player data!")
