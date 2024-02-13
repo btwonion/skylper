6222678a9370282e90ba8ce510191923bdb9eabe
btwonion <btwonion@gmx.de>
2024-02-13 16:50:11 +0100

    fix skylper gui command

--- src/main/kotlin/dev/nyon/skylper/skyblock/Command.kt
+++ src/main/kotlin/dev/nyon/skylper/skyblock/Command.kt
@@ -1,8 +1,24 @@
 package dev.nyon.skylper.skyblock
 
+import dev.nyon.skylper.config.createYaclScreen
+import dev.nyon.skylper.mcScope
+import dev.nyon.skylper.minecraft
 import dev.nyon.skylper.skyblock.hollows.appendCrystalHollowsSubCommand
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import net.silkmc.silk.commands.clientCommand
+import kotlin.time.Duration.Companion.milliseconds
 
 fun registerRootCommand() = clientCommand("skylper") {
     appendCrystalHollowsSubCommand()
+
+    literal("gui") {
+        runs {
+            mcScope.launch {
+                delay(100.milliseconds)
+                val screen = createYaclScreen(null)
+                minecraft.setScreen(screen)
+            }
+        }
+    }
 }
