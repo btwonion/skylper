657b95729bf52cc0c73f2ca632205678320c83bd
btwonion <btwonion@gmx.de>
2024-03-14 21:18:52 +0100

    fix conflict

--- src/main/kotlin/dev/nyon/skylper/extensions/Debug.kt
+++
@@ -1,7 +0,0 @@
-package dev.nyon.skylper.extensions
-
-import net.fabricmc.loader.api.FabricLoader
-
-fun debug(string: String) {
-    if (FabricLoader.getInstance().isDevelopmentEnvironment) println("[skylper] $string")
-}
--- src/main/kotlin/dev/nyon/skylper/extensions/EventHandler.kt
+++ src/main/kotlin/dev/nyon/skylper/extensions/EventHandler.kt
@@ -12,25 +12,6 @@ object EventHandler {
 
     private val listeners = mutableListOf<EventInstance<*, *>>()
 
-    private val debugIgnoredEvents: List<KClass<out Event<*>>> =
-        listOf(
-            TickEvent::class,
-            EntitySpawnEvent::class,
-            ParticleSpawnEvent::class,
-            MessageEvent::class,
-            RenderAfterTranslucentEvent::class,
-            RenderHudEvent::class,
-            BlockUpdateEvent::class,
-            BlockInteractEvent::class,
-            SetItemEvent::class,
-            ScreenOpenEvent::class,
-            InventoryInitEvent::class,
-            BossBarNameUpdate::class,
-            SideboardUpdateEvent::class,
-            RenderItemBackgroundEvent::class,
-            LocatedHollowsStructureEvent::class
-        )
-
     inline fun <reified E : Event<C>, C : Any?> listenEvent(noinline callback: (event: E) -> C) {
         val eventClass = E::class
         listenEvent(eventClass, callback)
@@ -57,7 +38,6 @@ object EventHandler {
                     it.invoke(event)
                 } ?: return@runBlocking null
 
-            if (!debugIgnoredEvents.contains(event::class)) debug("Invoked event $event for ${eventListeners.listeners.size} listeners.")
             return@runBlocking callbacks.firstOrNull()
         }
 }
