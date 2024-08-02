1b236beb554a643f6b7aafcadea9b37f55cccdef
btwonion <btwonion@gmx.de>
2024-08-02 16:20:02 +0200

    use CompactNumberFormat fix #30

--- beta-changelog.md
+++ beta-changelog.md
@@ -1,5 +1,6 @@
 ## Features
-none
+- only render hud widget in modify screen if they should be rendered in-game, too
+- use CompactNumberFormat for number formatting - numbers are now formatted with one fraction: 1,100,000 -> 1,1m
 
 ## Bug Fixes
 - fix misplaced waypoints in Crystal Hollows
--- src/main/kotlin/dev/nyon/skylper/extensions/math/Numbers.kt
+++ src/main/kotlin/dev/nyon/skylper/extensions/math/Numbers.kt
@@ -1,46 +1,13 @@
 package dev.nyon.skylper.extensions.math
 
-import java.util.*
+import java.text.NumberFormat
+import java.util.Locale
 
-private val suffixes = TreeMap<Long, String>().apply {
-    this[1000L] = "k"
-    this[1000000L] = "M"
-    this[1000000000L] = "B"
-    this[1000000000000L] = "T"
-    this[1000000000000000L] = "P"
-    this[1000000000000000000L] = "E"
+private val numberFormat = NumberFormat.getCompactNumberInstance(Locale.ROOT, NumberFormat.Style.SHORT).also {
+    it.minimumFractionDigits = 1
 }
 
-fun Number.format(preciseBillions: Boolean = false): String {
+fun Number.format(): String {
     val value = this.toLong()
-
-    if (value == Long.MIN_VALUE) return (Long.MIN_VALUE + 1).format(preciseBillions)
-    if (value < 0) return "-" + (-value).format(preciseBillions)
-
-    if (value < 1000) return value.toString()
-
-    val (divideBy, suffix) = suffixes.floorEntry(value)
-
-    val truncated = value / (divideBy / 10)
-
-    val truncatedAt = when (suffix) {
-        "M" -> {
-            1000
-        }
-        "B" -> {
-            1000000
-        }
-        else -> {
-            100
-        }
-    }
-
-    val hasDecimal = truncated < truncatedAt && truncated / 10.0 != (truncated / 10).toDouble()
-
-    return if (value > 1_000_000_000 && hasDecimal && preciseBillions) {
-        val decimalPart = (value % 1_000_000_000) / 1_000_000
-        "${truncated / 10}.$decimalPart$suffix"
-    } else {
-        if (hasDecimal) (truncated / 10.0).toString() + suffix else (truncated / 10).toString() + suffix
-    }
+    return numberFormat.format(value)
 }
