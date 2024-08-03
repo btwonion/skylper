3a6c97876c851cb7dac03e5f1b6f7d4c60b5d574
btwonion <btwonion@gmx.de>
2024-08-03 14:25:10 +0200

    improve item extensions

--- src/main/kotlin/dev/nyon/skylper/extensions/Items.kt
+++ src/main/kotlin/dev/nyon/skylper/extensions/Items.kt
@@ -1,61 +1,33 @@
-package dev.nyon.skylper.extensions/*? if >1.20.5 {*/
+package dev.nyon.skylper.extensions
+
 import dev.nyon.skylper.minecraft
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.Component
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
+
+/*? if >=1.20.6 {*/
 import net.minecraft.core.component.DataComponents
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.network.chat.Component
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.TooltipFlag
+/*?}*/
 
 val ItemStack.nameAsString: String
     get() {
         return displayName.string.replace("[", "").replace("]", "")
     }
 
-val ItemStack.extraAttributes: CompoundTag?
+val ItemStack.compoundTag: CompoundTag?
     get() {
-        val tag = get(DataComponents.CUSTOM_DATA)?.copyTag() ?: return null
-        val key = "ExtraAttributes"
-        if (!tag.contains(key)) return null
-        return tag.getCompound(key)
-    }
+        //? if >=1.20.6
+        return get(DataComponents.CUSTOM_DATA)?.copyTag() ?: return null
 
-val ItemStack.internalName: String?
-    get() {
-        val attributes = extraAttributes ?: return null
-        val key = "id"
-        if (!attributes.contains(key)) return null
-        return attributes.getString(key)
-    }
-
-val ItemStack.display: CompoundTag?
-    get() {
-        val tag = get(DataComponents.CUSTOM_DATA)?.copyTag() ?: return null
-        val key = "display"
-        if (!tag.contains(key)) return null
-        return tag.getCompound(key)
-    }
-
-val ItemStack.lore: List<Component>
-    get() {
-        val lines = getTooltipLines(Item.TooltipContext.EMPTY, minecraft.player!!, TooltipFlag.ADVANCED)
-        return lines.drop(1)
-    }/*?} else {*/
-/*import dev.nyon.skylper.minecraft
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.network.chat.Component
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.TooltipFlag
-
-val ItemStack.nameAsString: String
-    get() {
-        return displayName.string.replace("[", "").replace("]", "")
+        //? if <1.20.6
+        /*return tag*/
     }
 
 val ItemStack.extraAttributes: CompoundTag?
     get() {
-        tag ?: return null
-        val compoundTag = tag as CompoundTag
+        val compoundTag = compoundTag ?: return null
         val key = "ExtraAttributes"
         if (!compoundTag.contains(key)) return null
         return compoundTag.getCompound(key)
@@ -71,8 +43,7 @@ val ItemStack.internalName: String?
 
 val ItemStack.display: CompoundTag?
     get() {
-        tag ?: return null
-        val compoundTag = tag as CompoundTag
+        val compoundTag = compoundTag ?: return null
         val key = "display"
         if (!compoundTag.contains(key)) return null
         return compoundTag.getCompound(key)
@@ -80,7 +51,8 @@ val ItemStack.display: CompoundTag?
 
 val ItemStack.lore: List<Component>
     get() {
-        val lines = getTooltipLines(minecraft.player, TooltipFlag.ADVANCED)
+        val lines = getTooltipLines(/*? if >=1.20.6 {*/ Item.TooltipContext.EMPTY, /*?}*/ minecraft.player!!,
+            TooltipFlag.ADVANCED
+        )
         return lines.drop(1)
     }
-*//*?}*/
